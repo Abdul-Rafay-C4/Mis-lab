0468e1386cf5b6aee558b7270b17336bcb599fcf
Abdul Rafay <146338041+Abdul-Rafay-C4@users.noreply.github.com>
2023-11-23 17:11:25 +0500

    Update loyaltypoints.cpp
    adding 1 more thing

--- loyaltypoints.cpp
+++ loyaltypoints.cpp
@@ -23,10 +23,24 @@ using namespace std;
 				cin >> ch ;
 				if(ch == 'y' || ch == 'Y') // for discount
 				{
+					int userinp;
 					discount = (point / 10) * 5; // calculating discount
-					cout << "\nYou get discount of " << discount << "$" << endl;
-					cout << "\nYour total bill is :" << userspend - discount << "$" << endl; 
+					bool check;
+					do
+					{
+						check = true;
+						cout << "\nYou get discount of " << discount << "$" << " ,how much you want to spend : ";
+						cin >> userinp;
+						if(userinp > discount)
+						{
+							check = false; 
+							cout << "\nInvalid Input.";
+						}
+					}while(check == false);
 					point = 0;
+					discount = discount - userinp;
+					cout << "\nYour total bill is :" << userspend - userinp << "$" << endl; 
+					point = (discount * 10) / 5;
 				}
 				point = point + (userspend/10); // if user not spend loyalty points
 			}
